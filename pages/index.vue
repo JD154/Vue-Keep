b85eb83a977159f2d59308da2264a3c2d1f39cb6
Jesus Blanco <jesus.blancof96@gmail.com>
2020-01-14 15:09:08 -0400

    Remove local styles in order to make them global

--- pages/index.vue
+++ pages/index.vue
@@ -58,28 +58,5 @@ export default {
 
 <style lang="scss">
 
-  .user-thumbnail {
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-  }
-
-  .logo-wrapper {
-    display: flex;
-    align-items: flex-end;
-    justify-content: space-between;
-  }
-
-  .logo {
-    vertical-align: sub;
-    font-size: 1.7rem;
-    font-weight: 400;
-    margin: 0;
-
-    span {
-      font-weight: bold;
-    }
-  }
-
 </style>
 
